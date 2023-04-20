b83578b124a25aef420a8a1fd6fb45b330002f35
Szymon Jankowski <63104347+SJEnvi@users.noreply.github.com>
2023-04-20 20:05:21 +0200

    implemented form made by Bartek

--- deployment-structure/SEB-Project/main.py
+++ deployment-structure/SEB-Project/main.py
@@ -26,7 +26,35 @@ def form():
 
     return render_template('form.html')
 
+# function to add to JSON
 
+@app.route('/add_data', methods=['POST'])
+def add_data():
+    Longitude = request.form['Longitude']
+    Latitude = request.form['Latitude']
+    Name = request.form['Name']
+    Object_type = request.form['Object type']
+    
+
+    data = {
+        'Name': Name,
+        'Object type': Object_type,
+        'Latitude': float(Latitude), 
+        'Longitude': float(Longitude)
+                              
+        }
+    
+    with open ('dane1.json', 'r') as file:
+        exsisting_data = json.load(file)
+
+    exsisting_data.append(data)
+     
+    with open('dane1.json', 'w') as file:
+        json.dump(exsisting_data, file, indent=4)
+        file.write('\n')
+
+    return redirect('/')
+    
 if __name__ == '__main__':
 
     app.run(host='127.0.0.1', port=8080, debug=True)
